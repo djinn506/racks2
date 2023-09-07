a27e136ac232d06803b7cc459cda697972908580
djinn506 <djinn506@gmail.com>
2023-09-07 16:04:15 -0300

    Switch to .env credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Component } from './component/entities/component.entity';
 import { Server } from './server/entities/server.entity';
 
+const db_options = {
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      /*...db_options,*/
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'secret123!',
-      database: 'servers',
+      ...db_options,
       entities: [Component, Server],
       synchronize: true,
     }),
